50fe4c35edfd4837b370ca0789b34ef06fd30cc1
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-16 12:00:51 +0100

    actually stress

--- stressberry/main.py
+++ stressberry/main.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 #
+import subprocess
+from time import sleep
 
 
-def main():
+def start_stressing(num_cpus, time):
+
+    p = subprocess.Popen([
+        'stress',
+        '--cpu', str(num_cpus),
+        '--timeout', '{}s'.format(time)
+        ])
+
+    while p.poll() is None:
+        print('Still stressing', measure_temp())
+        sleep(1)
+
+    assert p.returncode == 0, \
+        'Error! (return code {})'.format(p.returncode)
+
+    # p = subprocess.Popen(
+    #     cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    #     )
+    # if verbose:
+    #     while True:
+    #         line = p.stdout.readline()
+    #         if not line:
+    #             break
+    #         print(line.decode('utf-8'), end='')
+
+    # p.communicate()
+    # assert p.returncode == 0, \
+    #     'Gmsh exited with error (return code {}).'.format(p.returncode)
     return
+
+
+def measure_temp():
+    p = subprocess.Popen('vcgencmd measure_temp', stdout=subprocess.PIPE)
+    output, _error = p.communicate()
+    return output.replace('temp=', '')
