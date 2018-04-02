8c17b8eb26389bfd440fc4f244680bed5674ada4
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-02 10:38:42 +0200

    version bump

--- stressberry/__about__.py
+++ stressberry/__about__.py
@@ -3,9 +3,8 @@
 
 __author__ = u'Nico Schlömer'
 __email__ = 'nico.schloemer@gmail.com'
-__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __email__)
-__credits__ = []
+__copyright__ = u'Copyright (c) 2017, {} <{}>'.format(__author__, __email__)
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __maintainer__ = 'Nico Schlömer'
 __status__ = 'Development Status :: 4 - Beta'
