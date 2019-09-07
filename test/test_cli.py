09e72c3772bbedb58e304599d07c255fd4a0c250
Martin Rowan <martin@rowannet.co.uk>
2019-09-07 12:00:28 +0100

    Add additional tests. Improve code coverage.

--- test/test_cli.py
+++ test/test_cli.py
@@ -12,10 +12,17 @@ def test_run():
     with open(frequency_file, "w") as f:
         f.write("1500000")
 
-    outfile = tempfile.NamedTemporaryFile().name
+    outfile1 = tempfile.NamedTemporaryFile().name
     stressberry.cli.run(
-        [outfile, "-t", temperature_file, "-f", frequency_file, "-d", "12", "-i", "3"]
+        [outfile1, "-d", "12", "-i", "2", "--cooldown", "1"]
+    )
+    
+    outfile2 = tempfile.NamedTemporaryFile().name
+    stressberry.cli.run(
+        [outfile2, "-t", temperature_file, "-f", frequency_file, "-d", "12", "-i", "2", "--cooldown", "1"]
     )
 
-    stressberry.cli.plot([outfile, "-f"])
+    stressberry.cli.plot([outfile1, "-f", "--temp-lims", "20", "90", "--freq-lims", "500", "2000"])
+    plotoutfile = tempfile.NamedTemporaryFile().name
+    stressberry.cli.plot([outfile2, "-f", "--temp-lims", "20", "90", "--freq-lims", "500", "2000", "-o", plotoutfile])
     return
