a796b870ab677cc4cf8722d8979f4ea4ba9f54cc
Martin Rowan <martin@rowannet.co.uk>
2019-09-14 09:26:45 +0100

    minor black fixes

--- helpers/show-metric.py
+++ helpers/show-metric.py
@@ -2,21 +2,24 @@ import argparse
 import glob
 import yaml
 
+
 def getFiles():
     filelist = [f for f in glob.glob("*.out", recursive=False)]
     return filelist
+
 
 def printAmbient(outFiles=None, metric=None):
     # csv header
     print("FileName,Metric,Min,Max,Var")
     # data
     for f in outFiles:
-        with open(f, 'r') as stream:
+        with open(f, "r") as stream:
             data = yaml.safe_load(stream)
         minimum = min(data[metric])
         maximum = max(data[metric])
         variance = maximum - minimum
         print("{},{},{},{},{}".format(f, metric, minimum, maximum, variance))
+
 
 def _get_parser_metrics():
     parser = argparse.ArgumentParser(description="CSV of basic metrics")
@@ -27,8 +30,9 @@ def _get_parser_metrics():
     )
     return parser
 
+
 if __name__ == "__main__":
-    argv=None
+    argv = None
     parser = _get_parser_metrics()
     args = parser.parse_args(argv)
     filelist = getFiles()
