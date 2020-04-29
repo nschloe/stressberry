923d5840838d6fb623ec3f23f87fc65d18884fd1
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-29 17:14:00 +0200

    update __about__

--- stressberry/__init__.py
+++ stressberry/__init__.py
@@ -1,21 +1,5 @@
 from . import cli
-from .__about__ import (
-    __author__,
-    __copyright__,
-    __email__,
-    __license__,
-    __status__,
-    __version__,
-)
+from .__about__ import __version__
 from .main import stress_cpu
 
-__all__ = [
-    "__author__",
-    "__email__",
-    "__copyright__",
-    "__license__",
-    "__version__",
-    "__status__",
-    "cli",
-    "stress_cpu",
-]
+__all__ = ["__version__", "cli", "stress_cpu"]
